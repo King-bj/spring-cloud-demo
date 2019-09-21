21eec6ebf1270d4c3038691905ddd354c2e28d85
king-bj <bjlajin@163.com>
2019-09-21 20:12:40 +0800

    rabbitmq demo第一版。

---
+++ rabbitmq/src/main/java/com/lajin/rabbitmq/util/Sender.java
@@ -0,0 +1,21 @@
+package com.lajin.rabbitmq.util;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消息发送者 * @author Administrator *
+ */
+@Component
+public class Sender {
+    @Autowired
+    private AmqpTemplate rabbitAmqpTemplate; /* * 发送消息的方法 */
+
+    //向消息队列发送消息
+    public void send(String msg) {
+        //参数一：队列的名称。 //参数二：消息
+        this.rabbitAmqpTemplate.convertAndSend("hello-queue",
+        msg);
+    }
+}
